0c0d71a57632c0f01cced32a20ebcd11bff736b1
tslee <tslee3375@naver.com>
2021-03-12 18:19:16 +0900

    Model, View, Controller

--- DotNetNote/Startup.cs
+++ DotNetNote/Startup.cs
@@ -12,6 +12,9 @@ namespace DotNetNote
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllersWithViews();
+
             //services.AddRazorPages();
         }
 
@@ -29,6 +32,9 @@ namespace DotNetNote
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute();
+                endpoints.MapDefaultControllerRoute();
+
                 //endpoints.MapRazorPages();
                 endpoints.MapGet("/", async context =>
                 {
